8cca7a7ba444c5ed789b1d296120791187e2aa16
dontharamakrishna <ram.dontha@viveninfomedia.com>
2017-10-11 13:06:58 +0530

    my first commit

---
+++ src/test/java/maven/first/AppTest.java
@@ -0,0 +1,26 @@
+package maven.first;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+ 
+public class AppTest 
+{
+	@Test
+	public void logintest()
+	{
+		WebDriver driver ;
+		
+		System.setProperty("webdriver.firefox.marionette", "H:\\Latest jars\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		
+		driver.get("https://github.com/SeleniumHQ/selenium/issues/3884");
+		driver.quit();
+		
+	}
+	
+
+
+}
