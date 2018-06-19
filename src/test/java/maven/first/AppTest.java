101702c0154cc25dda2d6ae73385bdaf212bd356
dontharamakrishna <ram.dontha@viveninfomedia.com>
2018-06-20 02:14:35 +0530

    new code

--- src/test/java/maven/first/AppTest.java
+++ src/test/java/maven/first/AppTest.java
@@ -32,7 +32,7 @@ public class AppTest {
 	          		 
         size = driver.findElements(By.xpath(".//*[@id='persons-image1']/a")).size();
         Purl= driver.getCurrentUrl();
-        
+        size =1;
       	 for(int i=1;i<=size;i++)
       	 {      
        	
@@ -44,7 +44,7 @@ public class AppTest {
   		     System.err.println(driver.findElement(By.xpath(".//*[@id='persons-image']")).getText());
   		     asizeonnextpage= driver.findElements(By.xpath("(.//*[@id='songs-list']/span/a)")).size();
       		 do {
-      			
+      			asizeonnextpage=1;
     //  		   System.out.println(asizeonnextpage);
       	      for(int j=1;j<=asizeonnextpage;j++)
       		  {
@@ -76,5 +76,68 @@ public class AppTest {
       	 
    	 
     }
+	 
+//    @Test
+	 public void seondww() throws InterruptedException
+	    {
+	   	 System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ramakrishna-PC\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	   	 
+//	   	 driver = new ChromeDriver();
+	   	 driver.get("http://rajamusicbank.com/person_type_new.php?alpbet=&action=Lyrics-Writers&id=6&start=0");
+	   	 String Purl;
+	   	 int size;
+	   	 int asizeonnextpage;
+	     driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+	   	 
+	   	do {
+		          		 
+	        size =1; //driver.findElements(By.xpath(".//*[@id='persons-image1']/a")).size();
+	        Purl= driver.getCurrentUrl();
+	        
+	      	 for(int i=1;i<=size;i++)
+	      	 {      
+	       	
+	      		driver.findElement(By.xpath("(.//*[@id='persons-image1']/a)["+i+"]")).click();
+	      		Thread.sleep(2000);
 
+	      		System.out.println("-----------***********----------");
+	      		 
+	  		     System.err.println(driver.findElement(By.xpath(".//*[@id='persons-image']")).getText());
+	  		     asizeonnextpage= driver.findElements(By.xpath("(.//*[@id='songs-list']/span/a)")).size();
+	      		 do {
+	      			
+	    //  		   System.out.println(asizeonnextpage);
+	      	      for(int j=1;j<=asizeonnextpage;j++)
+	      		  {
+	      		   System.out.println(driver.findElement(By.xpath("(.//*[@id='songs-list']/span/a)["+j+"]")).getAttribute("href"));
+	      		  }
+	      		
+	      		    Thread.sleep(2000);
+	      		    try{
+	         	    driver.findElement(By.xpath(".//td/a[contains(text(),'Next')]")).click();
+	      		    }
+	      		    catch(Exception e)
+	      		    {
+	      		    	System.out.println("only one page");
+	      		    }
+	      		    		
+	      		  } while(driver.findElements(By.xpath(".//td/a[contains(text(),'Next')]")).size()!=0);
+	      	  
+//	      		 System.out.println(Purl);
+	         	 driver.get(Purl);
+	   	     }
+	      	 
+	      	 
+	      	 Thread.sleep(2000);
+	      	 
+	      	 driver.findElement(By.xpath(".//td/a[contains(text(),'Next')]")).click();
+	       }while(driver.findElements(By.xpath(".//td/a[contains(text(),'Next')]")).size()!=0);
+	      	 
+//	   	driver.quit();
+	      	 
+	   	 
+	    }
+
+	 
+	 
 }
--- src/test/java/maven/first/secondtest.java
+++ src/test/java/maven/first/secondtest.java
@@ -1,23 +1,220 @@
 package maven.first;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Test;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import org.apache.commons.lang3.StringUtils;
+
+
 
 public class secondtest {
 
-	
-	WebDriver driver;
-	 @Test
-
-     public void seond()
+	  static String[] str= {"ram","sam"};
+	  static int count =0;
+	 static int[] ar = {1,2,3,4,5,6,7,8,9,10};
+     public static void main(String[] args) 
      {
-	  System.out.println("ramakrishna");
-    	 System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ramakrishna-PC\\Downloads\\chromedriver_win32\\chromedriver.exe");
+         String  str1 ="the bb the ccc cc bb";
+    	 System.out.println("String:\n" + str);
+ 		str1 = StringUtils.lowerCase(str1);
+ 		int countThe = StringUtils.countMatches(str1, "ccc");
+ 		System.out.println("\"the\" count: " + countThe);
     	 
-    	 driver = new ChromeDriver();
-    	 driver.get("https://www.guru99.com/using-apache-ant-with-selenium.html");
-    	 driver.quit();
+//    	 for(;;);
+//    	 System.out.println("java");
+    	 
+//    	 System.out.println(ar.length);
+//	   for(int i= 5;i<=ar.length-1;i++)
+//		System.out.println(ar[i]);	  
+//	    System.out.println("sdsd s");
+    	 
+//    	 System.out.println(str.length);
+//    	 for(int i=0;i<=str.length-1;i++)
+//    	 {
+//    		 System.out.println(str[i]);
+//    	 }
+//    	 for (String s: str)
+//    	 {
+//    		 System.out.println(s);
+//    	 }
+    	 
+    	 
+//    	 for(String  j:str)
+//    	 {
+//    		 System.out.println(j);
+//    	 }		 
+//    	 for(int  j:ar)
+//    	 {
+//    		
+//    		
+//    		 if(j>5)
+//    		 System.out.println(j);
+//    		 
+//    		
+//    	 }
+//    	 continue;
+//    	 int i=0;
+//    	 
+//    	 while(i<ar.length)
+//    	 {
+//
+//          
+//    		if(i>=5)    			
+//    			System.out.println(ar[i]);
+//
+//    		 i++;            		    		 
+//    	 }
+//    	int i=0; 
+//    do{
+//    	
+//    	
+//      }	 
+    	 
+//    	 1) Use enhanced for loop or classic for loop with lenth of array.
+//    	 2) Use Arrays.asList() to convert Array into ArrayList and than print
+//    	 3) Use Java 5 Arrays.toString() and Arrays.deepToString() methods
+//int c=2;
+//    	 int b=3;
+//    	 switch(b)
+//    	 
+//    	 {
+//    	 case 1: System.out.println("java");
+//    	
+//    	
+//    	 case 2: System.out.println("bava");
+//    	  break;
+//    	 
+//    	 default:System.out.println("kkk");
+//    	 
+//    	
+//    	 }
+//    	 char key = ' ';
+//
+//    			 switch(key) {
+//    			     case     ' '  :
+//    			     case     '\t' : System.out.println("white space char");
+//    			                    break;
+//
+//    			     default       : System.out.println("amount is something else");
+//    			 }
+// 
+    	 
+//    	 int[] ints   = {1,2,3,4,5};
+//
+//    	 int insertIndex = 2;
+//    	 int newValue    = 123;
+//
+//    	 //move elements below insertion point.
+//    	 for(int i=ints.length-1; i > insertIndex; i--){
+//    	     ints[i] = ints[i-1];
+//    	 }
+//
+//    	 //insert new value
+//    	 ints[insertIndex] = newValue;
+//
+//    	 System.out.println(Arrays.toString(ints));
+    	 
+//    	 int[] ints   = {1,2,3,4,5};
+//
+//    	 ints[3] = 123;
+//
+//    	 int removeIndex = 3;
+//
+//    	 for(int i = removeIndex; i < ints.length -1; i++){
+//    	     ints[i] = ints[i + 1];
+//    	 } 
+//    	 System.out.println(Arrays.toString(ints));
+  
+//    	 int[] ints = {0,2,9,6,11,6,9};
+    	
+
+//    	 int maxVal = Integer.MIN_VALUE;
+//    	 for(int i=0; i < ints.length; i++){
+//    	     if(ints[i] > maxVal){
+//    	         maxVal = ints[i];
+//    	     }
+//    	 }
+//    	 System.out.println("maxVal = " + maxVal);
+    	 String str = "java is java and is kova java";
+    	 String[] ints =str.split(" ");
+//    	 String[] ints={"abc","rr","rr","ryyy"};
+    	 int count =0;
+    	 System.out.println(ints.length);
+    	 for(int i=0;i<ints.length;i++)
+    	 {
+//    		 System.out.println(ints[i]);
+    		 for(int j=i+1;j<ints.length;j++)
+    		 {
+    		    
+    			 if(ints[i].equals(ints[j]))
+    			 {
+    				 System.out.println(ints[i]);
+    			 }
+    			
+    		 }
+    	 }
+    	 
+    	 
+    	 
+    	 int p = 0;
+    	 for( int i=1; i<=5; i++ ){
+    	 for( int k=1; k<=5-i; k++ ){
+    	 System.out.print (" ");
+    	 }
+    	 for( int j=1; j<=i+p; j++ ){
+    	 System.out.print ("*");
+    	 }
+    	 System.out.println ();
+    	 p=p+1;
+     
      }
+    	 
+    	 
+    	 
+//    	 Scanner in = new Scanner(System.in);
+//
+//    	 System.out.println("enter the num for which u want the factorial");
+//
+//    	 int num = in.nextInt();
+//
+//    	 for(int i=num-1; i>0; i-- ){
+//    		 System.out.println(num +" "+ i);
+//    	 num = num*i;
+//    	
+//    	 }
+//
+//    	 System.out.println(num);
+//
+//    	 }
+     
+    	 
+    	 String str3 = "I use selenium webdriver. selenium is a tool for web applications.";
+    	 int i = str3.length();
+    	 StringBuffer strb = new StringBuffer();
+    	 for( int j=i-1; j>=0; j--){
+    	 strb.append(str3.charAt(j));
+    	 }
+    	 System.out.println(strb);
+    	 
+     
+
+         int b=1885;
+
+         StringBuffer strb1 = new StringBuffer();
+          int rem;
+         while( b != 0 )
+         {
+               rem= b%10;
+               b=b/10;
+               strb1.append(rem);
+         }
+
+         System.out.println("Reverse of entered number is "+strb1);
+     
+     }
+     
+
+  
+     
+     
 }
